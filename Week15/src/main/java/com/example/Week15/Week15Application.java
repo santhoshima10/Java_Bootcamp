c2c4993ce15ea82517dbdbf5482a820f1bc74dd3
Vidhya Santhoshima <santhoshima.work@gmail.com>
2020-12-07 17:05:38 -0800

    Initial Commit

---
+++ Week15/src/main/java/com/example/Week15/Week15Application.java
@@ -0,0 +1,13 @@
+package com.example.Week15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week15Application.class, args);
+	}
+
+}
---
+++ Week15/src/test/java/com/example/Week15/Week15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Week15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
